bfca5196b5db3032df73d295c6915719cceccc59
jrecio <jrecio@solidgear.es>
2019-10-23 18:41:49 +0200

    finish basic project setup

--- src/test/java/io/cucumber/skeleton/StepDefinitions.java
+++ src/test/java/io/cucumber/skeleton/StepDefinitions.java
@@ -1,11 +1,23 @@
 package io.cucumber.skeleton;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
 
 public class StepDefinitions {
     @Given("I have {int} cukes in my belly")
     public void I_have_cukes_in_my_belly(int cukes) throws Throwable {
         Belly belly = new Belly();
         belly.eat(cukes);
+    }
+
+    @When("I wait {int} hour")
+    public void i_wait_hours(int hours) throws Throwable {
+        Thread.sleep(hours);
+    }
+
+    @Then("my belly should growl")
+    public void my_belly_should_growl() {
+        int a = 1+1;
     }
 }
