71928d5320cc5e4c31e08f0f71edf6d61b5b9c2d
jrecio <jrecio@solidgear.es>
2019-10-24 09:39:03 +0200

    add libraries; add setup

--- src/test/java/io/cucumber/skeleton/StepDefinitions.java
+++ src/test/java/io/cucumber/skeleton/StepDefinitions.java
@@ -1,10 +1,48 @@
 package io.cucumber.skeleton;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class StepDefinitions {
+
+    @Before
+    public void setup() throws MalformedURLException{
+
+        WebDriver driver;
+        WebDriverWait wait;
+        File rootPath = new File(System.getProperty("user.dir"));
+        File appDir = new File(rootPath,"src/test/resources");
+        File app = new File(appDir,"owncloud.apk");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability ( CapabilityType.PLATFORM, "Android");
+        capabilities.setCapability ("deviceName", "test");
+        capabilities.setCapability ("app", app.getAbsolutePath());
+        capabilities.setCapability ("appPackage", "com.owncloud.android");
+        capabilities.setCapability ("appActivity", ".ui.activity.FileDisplayActivity");
+        //capabilities.setCapability ("appWaitActivity", ".authentication.AuthenticatorActivity");
+
+        driver = new AndroidDriver (new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 5, 5);
+
+    }
+
     @Given("I have {int} cukes in my belly")
     public void I_have_cukes_in_my_belly(int cukes) throws Throwable {
         Belly belly = new Belly();
@@ -19,5 +57,10 @@ public class StepDefinitions {
     @Then("my belly should growl")
     public void my_belly_should_growl() {
         int a = 1+1;
+    }
+
+    @After
+    public void tearDown(){
+
     }
 }
