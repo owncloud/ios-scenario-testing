9b693cd3dd04de29fabc562ed130d2544cb98244
jrecio <jrecio@solidgear.es>
2019-11-05 17:42:29 +0100

    abstract pages for every step definition

--- src/test/java/io/cucumber/LoginSteps.java
+++ src/test/java/io/cucumber/LoginSteps.java
@@ -18,6 +18,11 @@ import static org.junit.Assert.assertTrue;
 
 public class LoginSteps {
 
+    //Involved pages
+    private WizardPage wizardPage;
+    private LoginPage loginPage;
+    private FileListPage fileListPage;
+
     private AndroidDriver driver;
     private final String serverURL = "http://10.40.40.198:17000";
 
@@ -26,18 +31,20 @@ public class LoginSteps {
         AppiumManager manager = new AppiumManager();
         manager.init();
         driver = manager.getDriver();
+
+        wizardPage = new WizardPage(driver);
+        loginPage = new LoginPage(driver);
+        fileListPage = new FileListPage(driver);
     }
 
     @Given("^I am a valid user$")
     public void i_am_a_valid_user() throws Throwable {
-        WizardPage wizardPage = new WizardPage(driver);
         wizardPage.skip();
     }
 
     @When("^I login as (.+) with password (.+)$")
     public void i_login_as_string_with_password_string(String username, String password)
             throws Throwable {
-        LoginPage loginPage = new LoginPage(driver);
         loginPage.typeURL();
         loginPage.typeCredentials(username, password);
         loginPage.allowPermissions();
@@ -46,20 +53,17 @@ public class LoginSteps {
     @When("^I login as (.+) with incorrect password (.+)$")
     public void i_login_as_string_with_incorrect_password_string(String username, String password)
             throws Throwable {
-        LoginPage loginPage = new LoginPage(driver);
         loginPage.typeURL();
         loginPage.typeCredentials(username, password);
     }
 
     @Then("^I can see the main page$")
     public void i_can_see_the_main_page() throws Throwable {
-        FileListPage fileListPage = new FileListPage(driver);
         assertTrue(fileListPage.isHeader());
     }
 
     @Then("^I see an error message$")
     public void i_see_an_error_message() throws Throwable {
-        LoginPage loginPage = new LoginPage(driver);
         assertTrue(loginPage.isCredentialsErrorMessage());
     }
 
--- src/test/java/io/cucumber/ShareSteps.java
+++ src/test/java/io/cucumber/ShareSteps.java
@@ -19,6 +19,12 @@ import static org.junit.Assert.assertTrue;
 
 public class ShareSteps {
 
+    //Involved pages
+    private WizardPage wizardPage;
+    private LoginPage loginPage;
+    private SharePage sharePage;
+    private FileListPage fileListPage;
+
     private AndroidDriver driver;
     private final String serverURL = "http://10.40.40.198:17000";
 
@@ -27,13 +33,16 @@ public class ShareSteps {
         AppiumManager manager = new AppiumManager();
         manager.init();
         driver = manager.getDriver();
+
+        wizardPage = new WizardPage(driver);
+        loginPage = new LoginPage(driver);
+        sharePage = new SharePage(driver);
+        fileListPage = new FileListPage(driver);
     }
 
     @Given("^I am logged$")
     public void i_am_logged() throws Throwable {
-        WizardPage wizardPage = new WizardPage(driver);
         wizardPage.skip();
-        LoginPage loginPage = new LoginPage(driver);
         loginPage.typeURL();
         loginPage.typeCredentials("user1", "a");
         loginPage.allowPermissions();
@@ -41,23 +50,18 @@ public class ShareSteps {
 
     @When("^I select (.+) to share with (.+)$")
     public void i_select_to_share_with(String itemName, String sharee) throws Throwable {
-        FileListPage fileListPage = new FileListPage(driver);
         fileListPage.shareAction(itemName);
-        SharePage sharePage = new SharePage(driver);
         sharePage.shareWith(sharee);
     }
 
     @When("^i select (.+) to create link with name (.+)$")
     public void i_select_to_link_with_name(String itemName, String name) throws Throwable {
-        FileListPage fileListPage = new FileListPage(driver);
         fileListPage.shareAction(itemName);
-        SharePage sharePage = new SharePage(driver);
         sharePage.shareLink(name);
     }
 
     @Then("^(.+) is shared with (.+)$")
     public void is_shared_with(String itenName, String sharee) throws Throwable {
-        SharePage sharePage = new SharePage(driver);
         sharePage.backListShares();
         assertTrue(sharePage.isItemInList(itenName));
         assertTrue(sharePage.isUserInList(sharee));
@@ -70,7 +74,6 @@ public class ShareSteps {
 
     @Then("^public link is created with the name (.+)")
     public void public_link_created(String name) throws Throwable {
-        SharePage sharePage = new SharePage(driver);
         assertTrue(sharePage.isNameInList(name));
     }
 
