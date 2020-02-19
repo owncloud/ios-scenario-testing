aa569ef5630422befc105f0206858d2cbe6a48b9
jrecio <jrecio@solidgear.es>
2020-02-19 08:32:53 +0100

    add chrome custom tab page

---
+++ src/main/java/android/ChromeCustomTab.java
@@ -0,0 +1,22 @@
+package android;
+
+public class ChromeCustomTab extends CommonPage {
+
+    private String username;
+    private String password;
+    private String acceptButton;
+    private String authorizeButton;
+
+    public ChromeCustomTab(){
+        super();
+    }
+
+    public void enterCredentials(){
+        //Type user and password, and click OK
+    }
+
+    public void authorize(){
+        //Click on Authorize
+    }
+
+}
