d87052d163120a3cabedd37ee4d05574971ef9b0
jrecio <jrecio@solidgear.es>
2020-11-12 14:43:37 +0100

    adapt kopano page to new view parameters

--- src/main/java/android/KopanoPage.java
+++ src/main/java/android/KopanoPage.java
@@ -1,55 +1,93 @@
 package android;
 
 import org.openqa.selenium.By;
-
 import java.util.logging.Level;
-
 import utils.log.Log;
 
 public class KopanoPage extends CommonPage {
 
+    private long deviceVersion;
+
+    private String username_xpath_10 = "/hierarchy/android.widget.FrameLayout/android.widget." +
+            "LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget." +
+            "FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.widget." +
+            "FrameLayout[2]/android.webkit.WebView/android.view.View/android.view.View[1]/android.view." +
+            "View/android.widget.EditText[1]";
+
     private String username_xpath = "/hierarchy/android.widget.FrameLayout/android.widget." +
+            "LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget." +
+            "FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.widget." +
+            "FrameLayout[1]/android.webkit.WebView/android.view.View/android.view.View[1]/android.view." +
+            "View/android.widget.EditText[1]";
+
+    private String password_xpath_10 = "/hierarchy/android.widget.FrameLayout/android.widget." +
+            "LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget." +
+            "FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.widget." +
+            "FrameLayout[2]/android.webkit.WebView/android.view.View/android.view.View[1]/android.view." +
+            "View/android.widget.EditText[2]";
+
+    private String password_xpath = "/hierarchy/android.widget.FrameLayout/android.widget." +
+            "LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget." +
+            "FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.widget." +
+            "FrameLayout[1]/android.webkit.WebView/android.view.View/android.view.View[1]/android.view." +
+            "View/android.widget.EditText[2]";
+
+    private String acceptButton_xpath_10 = "/hierarchy/android.widget.FrameLayout/android.widget." +
+            "LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget." +
+            "FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.widget." +
+            "FrameLayout[2]/android.webkit.WebView/android.view.View/android.view.View[1]/android.view." +
+            "View/android.view.View/android.widget.Button";
+
+    private String acceptButton_xpath = "/hierarchy/android.widget.FrameLayout/android.widget." +
+            "LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget." +
+            "FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.widget." +
+            "FrameLayout[1]/android.webkit.WebView/android.view.View/android.view.View[1]/android.view." +
+            "View/android.view.View/android.widget.Button";
+
+    private String authorize_xpath_10 = "/hierarchy/android.widget.FrameLayout/android.widget." +
+            "LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget." +
+            "FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.widget." +
+            "FrameLayout[2]/android.webkit.WebView/android.view.View/android.view.View[1]/android.view." +
+            "View[5]/android.view.View[2]/android.widget.Button";
+
+    private String authorize_xpath = "/hierarchy/android.widget.FrameLayout/android.widget." +
+            "LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget." +
+            "FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.widget." +
+            "FrameLayout[1]/android.webkit.WebView/android.view.View/android.view.View[1]/android.view." +
+            "View[5]/android.view.View[2]/android.widget.Button";
+
+
+    //old stuff for Android older than 10
+    /*private String username_xpath_old = "/hierarchy/android.widget.FrameLayout/android.widget." +
             "LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget." +
             "FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.widget." +
             "FrameLayout[1]/android.webkit.WebView/android.view.View/android.app.Dialog/android.view." +
             "View/android.view.View/android.view.View[1]/android.view.View[2]/android.view." +
             "View[3]/android.view.View[1]/android.view.View[2]/android.widget.EditText";
 
-    //private String username_xpath = "//android.webkit.WebView[@content-desc=\"Kopano Sign in\"]/android.app.Dialog/android.view.View/android.view.View/android.view.View[1]/android.view.View[2]/android.view.View[3]/android.view.View[1]/android.view.View[2]/android.widget.EditText";
-
-
-    private String password_xpath = "/hierarchy/android.widget.FrameLayout/android.widget." +
+    private String password_xpath_old = "/hierarchy/android.widget.FrameLayout/android.widget." +
             "LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget." +
             "FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.widget." +
             "FrameLayout[1]/android.webkit.WebView/android.view.View/android.app.Dialog/android.view." +
             "View/android.view.View/android.view.View[1]/android.view.View[2]/android.view." +
             "View[3]/android.view.View[2]/android.view.View[2]/android.widget.EditText";
 
-    //private String password_xpath = "//android.webkit.WebView[@content-desc=\"Kopano Sign in\"]/android.app.Dialog/android.view.View/android.view.View/android.view.View[1]/android.view.View[2]/android.view.View[3]/android.view.View[2]/android.view.View[2]/android.widget.EditText";
-
-
-    private String acceptButton_xpath = "/hierarchy/android.widget.FrameLayout/android.widget." +
+    private String acceptButton_xpath_old = "/hierarchy/android.widget.FrameLayout/android.widget." +
             "LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget." +
             "FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.widget." +
             "FrameLayout[1]/android.webkit.WebView/android.view.View/android.app.Dialog/android.view." +
             "View/android.view.View/android.view.View[1]/android.view.View[2]/android.view." +
             "View[3]/android.view.View[3]/android.widget.Button";
 
-    //accesibility id = NEXT
-    //private String acceptButton_xpath = "//android.widget.Button[@content-desc=\"NEXT \"]";
-
-
-    private String authorize_xpath = "/hierarchy/android.widget.FrameLayout/android.widget." +
+    private String authorize_xpath_old = "/hierarchy/android.widget.FrameLayout/android.widget." +
             "LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget." +
             "FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.widget." +
             "FrameLayout[1]/android.webkit.WebView/android.view.View/android.app.Dialog/android." +
             "view.View/android.view.View/android.view.View[1]/android.view.View[2]/android.view." +
-            "View[6]/android.view.View[2]/android.widget.Button";
+            "View[6]/android.view.View[2]/android.widget.Button";*/
 
-    //accesibility id = ALLOW / CANCEL
-    //private String authorize_xpath = "//android.widget.Button[@content-desc=\"ALLOW \"]";
-
-    private String username_xpath_10 = "/hierarchy/android.widget.FrameLayout/android.widget." +
+    //old stuff for Android 10
+    /*private String username_xpath_10 = "/hierarchy/android.widget.FrameLayout/android.widget." +
             "LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget." +
             "FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.widget." +
             "FrameLayout[2]/android.webkit.WebView/android.view.View/android.app.Dialog/android.view." +
@@ -75,9 +113,8 @@ public class KopanoPage extends CommonPage {
             "FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.widget." +
             "FrameLayout[2]/android.webkit.WebView/android.view.View/android.app.Dialog/android." +
             "view.View/android.view.View/android.view.View[1]/android.view.View[2]/android.view." +
-            "View[6]/android.view.View[2]/android.widget.Button";
+            "View[6]/android.view.View[2]/android.widget.Button";*/
 
-    private long deviceVersion;
 
     public KopanoPage(){
         deviceVersion = (long) driver.getCapabilities().getCapability("deviceApiLevel");
@@ -88,7 +125,7 @@ public class KopanoPage extends CommonPage {
         if (deviceVersion >= 29) {
             Log.log(Level.FINE, "Android 10");
             if (!driver.findElementsByXPath(username_xpath_10).isEmpty()) {
-                Log.log(Level.FINE, "Entering credentials10");
+                Log.log(Level.FINE, "Entering credentials");
                 driver.findElement(By.xpath(username_xpath_10)).sendKeys(username);
                 driver.findElement(By.xpath(password_xpath_10)).sendKeys(password);
                 driver.findElement(By.xpath(acceptButton_xpath_10)).click();
