428ac01ca781d7dd544860d2218480e894766fbc
Jesus Recio <jrecio@izertis.com>
2026-04-27 14:25:17 +0200

    chore: update script to buiod the app in QA mode

--- buildapp/buildapp.sh
+++ buildapp/buildapp.sh
@@ -12,9 +12,16 @@ cd $PROJECT_ROOT
 
 gsed -i 's/.showBetaWarning : true/.showBetaWarning : false/i' ownCloudAppShared/Tools/VendorServices.swift
 grep .showBetaWarning ownCloudAppShared/Tools/VendorServices.swift
+gsed -i 's/.isBetaBuild : false/.isBetaBuild : true/i' ownCloudAppShared/Tools/VendorServices.swift
+grep .isBetaBuild ownCloudAppShared/Tools/VendorServices.swift
+gsed -i 's/value: OCLicenseQAProvider.isQAUnlockEnabled,/value: true,/i' ownCloud/Settings/DisplaySettingsSection.swift
+grep .isQAUnlockEnabled ownCloud/Settings/DisplaySettingsSection.swift
 gsed -i '170,200d' ownCloud/Release\ Notes/ReleaseNotesHostViewController.swift
 grep -C 2 shouldShowReleaseNotes ownCloud/Release\ Notes/ReleaseNotesHostViewController.swift
-gsed -i '136i OCConnectionAllowedAuthenticationMethodIDs      : @[ OCAuthenticationMethodIdentifierBasicAuth ],' ios-sdk/ownCloudSDK/Connection/OCConnection.m
+gsed -i '144i OCConnectionAllowedAuthenticationMethodIDs      : @[ OCAuthenticationMethodIdentifierBasicAuth ],' ios-sdk/ownCloudSDK/Connection/OCConnection.m
+gsed -i 's|self?\.expirationDate = \.now\.addingTimeInterval(24 \* 3600 \* 7)|self?.expirationDate = Calendar.current.date(byAdding: .day, value: 1, to: Calendar.current.startOfDay(for: .now))?.addingTimeInterval(-1)|' ownCloudAppShared/Client/Sharing/ShareViewController.swift
+grep Calendar.current.startOfDay ownCloudAppShared/Client/Sharing/ShareViewController.swift
+gsed -i 's|self?.expirationDate = expirationDate.addingTimeInterval(7 \* 24 \* 60 \* 60)|self?.expirationDate = Calendar.current.date(byAdding: .day, value: 1, to: Calendar.current.startOfDay(for: .now))?.addingTimeInterval(-1)|' ownCloudAppShared/Client/Sharing/ShareViewController.swift
 
 xcrun xcodebuild clean -scheme "ownCloud"
 
@@ -22,9 +29,8 @@ xcrun xcodebuild \
   -workspace ownCloud.xcworkspace \
   -scheme ownCloud \
   -configuration Debug \
-  -destination 'platform=iOS Simulator,name=iPhone 15'
-
+  -destination "platform=iOS Simulator,name=iPhone 17e,OS=26.4"
+  OTHER_SWIFT_FLAGS="-no-explicit-module-build"
 
 cp -r $APP_PATH/ownCloud.app $APP_TARGET
 rm -rf $APP_PATH
-
